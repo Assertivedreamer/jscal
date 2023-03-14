d690859f39ec3f3403881f9d16e581edae47d5c3
Assertivedreamer <assertivedreamer@gmail.com>
2023-03-14 18:51:42 +0530

    first commit

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ buttons.forEach((item) => {
       display.innerText = "";
     } else if (item.id == "backspace") {
       let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
+      display.innerText = string.substr(0, string.length - string.length);
     } else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
-    } else {
+    
+  } 
+    else {
       display.innerText += item.id;
     }
   };
